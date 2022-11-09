5612c05c6b31e7e5770b30e999be87cb476572c8
ManikGarg316 <navigarg779@gmail.com>
2022-11-10 01:50:13 +0530

    numerical integration codes

---
+++ ODEs.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double f(double x, double y)
+{
+    return 4*pow(x, 3) + 3*x*x + 2*x + 1;
+}
+
+double RK4(double h, double initial_val, double start, double find)
+{
+    int n = (find - start)/h;
+    // cout << n << '\n';
+    for(int i=0;i<n;i++)
+    {
+        double k1 = f(start, initial_val);
+        double k2 = f(start+h/2, initial_val + (h*k1)/2);
+        double k3 = f(start+h/2, initial_val + (h*k2)/2);
+        double k4 = f(start+h, initial_val + h*k3);
+        initial_val = initial_val + (h/6)*(k1+2*k2+2*k3+k4);
+        start = start+h;
+    }
+    return initial_val;
+}
+double RalstonMethod(double h, double initial_val, double start, double find)
+{
+    int n = (find - start)/h;
+    // cout << n << '\n';
+    for(int i=0;i<n;i++)
+    {
+        double k1 = f(start, initial_val);
+        double k2 = f(start+(3*h)/4, initial_val + (3*k1*h)/4);
+        initial_val = initial_val + (h/3)*(k1+2*k2);
+        start = start+h;
+    }
+    return initial_val;
+}
+
+double MidpointMethod(double h, double initial_val, double start, double find)
+{
+    int n = (find - start)/h;
+    // cout << n << '\n';
+    for(int i=0;i<n;i++)
+    {
+        double k1 = f(start, initial_val);
+        double k2 = f(start+h/2, initial_val + (1/2)*k1*h);
+        initial_val = initial_val + h*k2;
+        start = start+h;
+    }
+    return initial_val;
+}
+
+double HeunMethod(double h, double initial_val, double start, double find)
+{
+    int n = (find - start)/h;
+    // cout << n << '\n';
+    for(int i=0;i<n;i++)
+    {
+        double k1 = f(start, initial_val);
+        double k2 = f(start+h, initial_val + k1*h);
+        initial_val = initial_val + (h/2)*(k1+k2);
+        start = start+h;
+    }
+    return initial_val;
+}
+
+double Euler(double h, double initial_val, double start, double find)
+{
+    int n = (find - start)/h;
+    // cout << n << '\n';
+    for(int i=0;i<n;i++)
+    {
+        initial_val = initial_val + h*f(start, initial_val);
+        start = start+h;
+    }
+    return initial_val;
+}
+
+int main()
+{
+    // cout << Euler(10.0, 0.0, 0.0, 10.0) << '\n';
+    // cout << Euler(1.0, 0.0, 0.0, 10.0) << '\n';
+    // cout << Euler(0.1, 0.0, 0.0, 10.0) << '\n';
+    // cout << "\n\n";
+    // // cout << Euler(0.01, 0.0, 0.0, 10.0) << '\n';
+    // // cout << Euler(0.001, 0.0, 0.0, 10.0) << '\n';
+    // cout << HeunMethod(10.0, 0.0, 0.0, 10.0) << '\n';
+    // cout << HeunMethod(1.0, 0.0, 0.0, 10.0) << '\n';
+    // cout << HeunMethod(0.1, 0.0, 0.0, 10.0) << '\n';
+    // cout << "\n\n";
+    // cout << MidpointMethod(10.0, 0.0, 0.0, 10.0) << '\n';
+    // cout << MidpointMethod(1.0, 0.0, 0.0, 10.0) << '\n';
+    // cout << MidpointMethod(0.1, 0.0, 0.0, 10.0) << '\n';
+    // cout << "\n\n";
+    // cout << RalstonMethod(10.0, 0.0, 0.0, 10.0) << '\n';
+    // cout << RalstonMethod(1.0, 0.0, 0.0, 10.0) << '\n';
+    // cout << RalstonMethod(0.1, 0.0, 0.0, 10.0) << '\n';
+    // cout << "\n\n";
+    // cout << RK4(10.0, 0.0, 0.0, 10.0) << '\n';
+    // cout << RK4(1.0, 0.0, 0.0, 10.0) << '\n';
+    // cout << RK4(0.1, 0.0, 0.0, 10.0) << '\n';
+}
---
+++ integration.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double f(double x)
+{
+    return pow(x, 4.0) - pow(x, 3.0) + pow(x, 2.0) - x + (double)1;
+    // double temp = 1.0;
+    // double ans = 1.0;
+    // for(int i=0;i<4;i++)
+    // {
+    //     temp = temp*(-x);
+    //     ans += temp;
+    // }
+    // return ans;
+}
+
+vector<double> Points_Generator(double x0, double h, int n)
+{
+    vector<double> ans;
+    for(int i=0;i<n;i++)
+    {
+        ans.push_back(f(x0 + ((double)i)*h));
+    }
+    return ans;
+}
+
+double exact(double x)
+{
+    return ((double)4)*pow(x, 3.0) - ((double)3)*pow(x, 2.0) + ((double)2)*x - ((double)1);
+}
+
+vector<int> nextPascal(vector<int> vals)
+{
+    int n = vals.size();
+    vector<int> ans;
+    ans.push_back(1);
+    for(int i=0;i<n-1;i++)
+    {
+        ans.push_back(abs(vals[i])+abs(vals[i+1]));
+    }
+    ans.push_back(1);
+    for(int i=1;i<=n;i+=2)
+    {
+        ans[i] = -ans[i];
+    }
+    return ans;
+}
+
+double NewtonForwardScheme_FirstOrder(double x0, double h, int order_of_accuracy)
+{
+    double ans = 0.0;
+    vector<double> y = Points_Generator(x0, h, order_of_accuracy+1);
+   
+    vector<int> Level{1};
+    for(int i=0;i<order_of_accuracy;i++)
+    {
+        Level = nextPascal(Level);
+        double temp = 0.0;
+        for(int j=0;j<Level.size();j++)
+        {
+            temp = temp + ((double)Level[j])*y[j];
+        }
+        temp = temp/(double)(i+1);
+        if((i+1)%2 == 0)
+        {
+            ans -= temp;
+        }
+        else
+        {
+            ans += temp;
+        }
+    }
+    return ans/h;
+}
+
+double Trapezoidal(vector<pair<double, double>> data, double h)
+{
+    int n = data.size();
+    double ans = 0.0;
+    for(int i=1;i<n-1;i++)
+    {
+        ans = ans + ((double)2)*data[i].second;
+    }
+    ans = ans + data[0].second + data[n-1].second;
+    return (ans*h)/((double)2);
+}
+
+double SimpsonOneThird(vector<pair<double, double>> data, double h)
+{
+    int n = data.size();
+    double ans = 0.0;
+    if(n<3)
+    {
+        cout << "Cannot do it! Not enough points!";
+        exit(0);
+    }
+    for(int i=0;i<n-2;i+=2)
+    {
+        ans = ans + data[i].second + ((double)4)*data[i+1].second + data[i+2].second;
+    }
+    if(n%3 == 1)
+    {
+        ans = ans/((double)3) + (data[n-2].second + data[n-1].second)/((double)2);
+    }
+    else
+    {
+        ans = ans/((double)3);
+    }
+    return ans*h;
+}
+
+double SimpsonThreeEighth(vector<pair<double, double>> data)
+{
+    int n = data.size();
+    double ans = 0.0;
+    if(n<4)
+    {
+        cout << "Cannot do it! Not enough points!";
+        exit(0);
+    }
+    for(int i=0;i<n-3;i+=3)
+    {
+        ans = ans + data[i].second + ((double)3)*data[i+1].second + ((double)3)*data[i+2].second + data[i+3].second;
+    }
+    if(n%4 == 1)
+    {
+        ans = (ans*(double)3)/((double)8) + (data[n-2].second + data[n-1].second)/((double)2);
+    }
+    else if(n%4 == 2)
+    {
+        ans = (ans*(double)3)/((double)8) + (data[n-3].second + ((double)4)*data[n-2].second + data[n-1].second)/((double)3);
+    }
+    else
+    {
+        ans = (ans*(double)3)/((double)8);
+    }
+    return ans*h;
+}
+
+int main() {
+    // cout << exact(5.0) << '\n';
+    // cout << NewtonForwardScheme_FirstOrder(5.0, 0.0001, 100);
+    vector<pair<double, double>> data_points
+    {
+        {0,-1},
+        {1,2},
+        {2,23},
+        {3,86},
+        {4,215},
+        {5,434},
+        {6,767},
+        {7,1238},
+        {8,1871},
+        {9,2690},
+        {10,3719}
+    };
+    // cout << Trapezoidal(data_points) << '\n';
+    cout << SimpsonOneThird(data_points) << '\n';
+}
