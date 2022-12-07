ac7519461fd3a38b47e040ff547ac062846a20f3
ManikGarg316 <navigarg779@gmail.com>
2022-12-07 23:50:27 +0530

    Add relaxed parameter in fixed point iteration

---
+++ Fixed_Point_Iter_Relaxed.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<fstream>
+using namespace std;
+
+double g(double x)
+{
+    return (double)1 + ((double)1)/x;
+}
+
+void Fixed_Point_Iteration_Relax(double guess, double epsilon, double relaxParameter)
+{
+    ofstream OUTPUT("./output.txt");
+    long long int i=0;
+    long long int max_iter = 1000000000;
+    while(1)
+    {
+        if(i > max_iter)
+        {
+            OUTPUT<<"ERROR: Could not complete operation\nIterations exceeded\nDiverging answer\n";
+            OUTPUT<<"This is the end result by newton raphson method: "<<to_string(guess);
+            break;
+        }
+
+        double value = g(guess);
+        double temp = relaxParameter*guess + (1-relaxParameter)*value;
+        if(abs(temp - guess) <= epsilon)
+        {
+            OUTPUT<<"This is the root by fixed point iteration method: "<<to_string(temp);
+            break;
+        }
+        guess = temp;
+        i++;
+    }
+    OUTPUT.close();
+    return;
+}
+int main()
+{
+    double error = 0.0000001;
+    Fixed_Point_Iteration_Relax((double)(2), error, 0.2);
+}
